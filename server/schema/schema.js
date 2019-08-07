dc3d4c8fc6739cf707c929ca0b71085638b132f6
chinu1996 <pkd1964@gmail.com>
2019-08-07 18:46:02 +0530

    Added GraphQLList

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,53 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } =graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } =graphql;
+
+//Dummy data for the books
+const books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
+];
+
+var authors = [
+    { name: 'Iceberg slim', age: 44, id: '1'},
+    { name: 'Alok chandra', age: 45, id: '2'},
+    { name: 'Sidney Sheldon', age: 30, id: '3'}
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(authors, { id:parent.authorId })
+            }
+        }
+
+    })
+});
+
+const AuthorType= new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return _.filter(books, {authorId: parent.id});
+            }
+        }
 
     })
 });
@@ -18,13 +58,22 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book:{
             type: BookType,
-            args: {id : { type: GraphQLString}},
+            args: {id : { type: GraphQLID }},
             resolve(parent, args){
 
                 //Code to access the DB will go here
+                return _.find(books, { id:args.id });
 
             }
+        },
+        author:{
+            type:AuthorType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args){
+                return _.find(authors, {id:args.id });
+            }
         }
+
     }
 });
 
